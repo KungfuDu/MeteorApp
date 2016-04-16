fa536073c412bc7e5200f3e76e6c644c8b4ab939
Quentin Maréchal <qmarechal@hotmail.fr>
2016-04-16 13:56:39 +0100

    login page CSS
    material like ~ :+1:

--- client/login.js
+++ client/login.js
@@ -1,7 +1,7 @@
 // Login Modal --------------------------------------------------
 
 Template.modalLogin.helpers({
-  'showModal': function(){
+    'showModal': function() {
 
         return Session.get('showModal');
     }
@@ -9,83 +9,93 @@ Template.modalLogin.helpers({
 });
 
 Template.modalLogin.events({
-  'click #createAccountLink': function(){
-      Session.set('showModal', true);
-  },
-  'click #loginLink': function(){
-      Session.set('showModal', false);
-  },
-  submit : function(event){
-    event.preventDefault();
-    user=$('#username').val();
-    console.log(event);
+    'click #createAccountLink': function() {
+        Session.set('showModal', true);
+    },
+    'click #loginLink': function() {
+        Session.set('showModal', false);
+    },
+    submit: function(event) {
+        event.preventDefault();
+        user = $('#username').val();
+        console.log(event);
 
-  },
-  'click button':function(){
-    console.log('pute');
-    $('#modalLogin').hide();
-    Router.go('/home');
+    },
+    'click button': function() {
+        console.log('pute');
+        $('#modalLogin').hide();
+        Router.go('/home');
 
-  }
+    },
+    'focus .field-input': function(e){
+      console.log(222);
+      $(e.target).parent().addClass('is-focused has-label');
+    },
+    'blur .field-input': function(e){
+
+      $parent =  $(e.target).parent();
+      $parent.removeClass('is-focused');
+      if($(e.target).val()===""){
+        $parent.removeClass('has-label');
+      }
+    }
 });
+
 
 
 Template.login.events({
     "submit form": function(event) {
-		event.preventDefault();
-		var user = $("input[name='username']").val();
-		var password = $("input[name='password']").val();
+        event.preventDefault();
+        var user = $("input[name='username']").val();
+        var password = $("input[name='password']").val();
 
-	  // Login en utilisant le nom d'utilisateur ou l'email
-		Meteor.loginWithPassword(
-			user,
-			password,
-			function(err) {
-				if (err) {
-					alert(err.reason);
-				}
-        else{
-          Router.go('/home');
+        // Login en utilisant le nom d'utilisateur ou l'email
+        Meteor.loginWithPassword(
+            user,
+            password,
+            function(err) {
+                if (err) {
+                    alert(err.reason);
+                } else {
+                    Router.go('/home');
 
-        }
-			}
-		);
+                }
+            }
+        );
 
-	}
+    }
 });
 
 
 
 Template.register.events({
-  "submit form": function(event, template) {
-		event.preventDefault();
-		var username = $('input[name="username"]').val();
-		var email = $('input[name="email"]').val();
-		var password = $('input[name="password"]').val();
-		var user = {
-			username: username,
-			email: email,
-			password: password,
-      profile: {}
-		};
-		Accounts.createUser(user, function(err) {
-			if (err) {
-				alert(err.reason);
-			}
-      else{
-        Meteor.loginWithPassword(
-          user,
-          password,
-          function(err) {
+    "submit form": function(event, template) {
+        event.preventDefault();
+        var username = $('input[name="username"]').val();
+        var email = $('input[name="email"]').val();
+        var password = $('input[name="password"]').val();
+        var user = {
+            username: username,
+            email: email,
+            password: password,
+            profile: {}
+        };
+        Accounts.createUser(user, function(err) {
             if (err) {
-              console.log(err.reason);
+                alert(err.reason);
+            } else {
+                Meteor.loginWithPassword(
+                    user,
+                    password,
+                    function(err) {
+                        if (err) {
+                            console.log(err.reason);
+                        } else {
+                            Router.go('/home');
+                        }
+                    }
+                );
             }
-            else{
-              Router.go('/home');
-            }
-          }
-        );
-      }
-		});
-	}
+        });
+    }
 });
--- client/player.js
+++ client/player.js
@@ -1,15 +1,15 @@
 Meteor.subscribe("PlayerSettings");
 var localSettings = '';
-var player=null;
+var player = null;
 var rdy = false;
 var time_update;
 
-Template.videoPlayer.created = function(){
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  console.log('ok wesh');
+Template.videoPlayer.created = function() {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    console.log('ok wesh');
 };
 
 
@@ -35,271 +35,280 @@ Template.videoPlayer.created = function(){
 
 Template.videoPlayer.events({
 
-  "click #initRoom": function(e){
-    e.preventDefault();
-    Meteor.call('initRoom');
-  },
-  "click #buttonAdd" :function(e){
-    e.preventDefault();
+    "click #initRoom": function(e) {
+        e.preventDefault();
+        Meteor.call('initRoom');
+    },
+    "submit #addVideoForm": function(e) {
+        e.preventDefault();
 
-    var res = $('#nextInput').val();
-    $('#nextInput').val('');
-    console.log('addVideo');
-     Meteor.call('addVideo',{url : res});
-  },
-  "click #buttonPlay" :function(e){
-    e.preventDefault();
-    Meteor.call('playVideo');
-    console.log(player.getPlayerState());
-  },
-  "click #buttonNext" :function(e){
-    e.preventDefault();
-    //Meteor.clearInterval(time_update);
-    console.log(player);
-    //player = null;
-    //Meteor.call('finishVideo');
-     Meteor.call('nextVideo');
+        var res = $('#nextInput').val();
+        $('#nextInput').val('');
+        console.log('addVideo');
+        if (res !== '') {
+            Meteor.call('addVideo', {
+                url: res
+            });
+        }
+    },
+    "click #buttonPlay": function(e) {
+        e.preventDefault();
+        Meteor.call('playVideo');
+        console.log(player.getPlayerState());
+    },
+    "click #buttonNext": function(e) {
+        e.preventDefault();
+        //Meteor.clearInterval(time_update);
+        console.log(player);
+        //player = null;
+        //Meteor.call('finishVideo');
+        Meteor.call('nextVideo');
 
-  },
-  "click #buttonPause" :function(e){
-    e.preventDefault();
+    },
+    "click #buttonPause": function(e) {
+        e.preventDefault();
 
-    var time = player.getCurrentTime();
-     //Meteor.clearInterval(time_update);
-     Meteor.call('pauseVideo',time);
-  },
-  "touchend, mouseup #progress-bar" : function(e){
-    var bar = e.target.value;
-    if(player){
-      if(bar !== ''){
-        var newTime = player.getDuration()*(bar/ 100);
-        console.log(newTime);
-        // Skip video to new time.
-        Meteor.call('changeTime',newTime);
-      }
+        var time = player.getCurrentTime();
+        //Meteor.clearInterval(time_update);
+        Meteor.call('pauseVideo', time);
+    },
+    "touchend, mouseup #progress-bar": function(e) {
+        var bar = e.target.value;
+        if (player) {
+            if (bar !== '') {
+                var newTime = player.getDuration() * (bar / 100);
+                console.log(newTime);
+                // Skip video to new time.
+                Meteor.call('changeTime', newTime);
+            }
+        }
+
+    },
+    "touchend, mouseup #volume-bar": function(e) {
+        var bar = e.target.value;
+        if (player) {
+            if (bar !== '') {
+                player.setVolume(bar);
+
+            }
+        }
+
+    },
+    "load #player": function(e) {
+
+        e.preventDefault();
+
     }
-
-  },
-  "touchend, mouseup #volume-bar" : function(e){
-    var bar = e.target.value;
-    if(player){
-      if(bar !== ''){
-        player.setVolume(bar);
-
-      }
-    }
-
-  },
-  "load #player": function(e){
-
-    e.preventDefault();
-
-   }
 
 });
 
 Template.videoPlayer.helpers({
-    room : function(){
+    room: function() {
 
-      var newSettings = Settings.findOne({param:"default"});
+        var newSettings = Settings.findOne({
+            param: "default"
+        });
 
-      if (newSettings) {
-        updateSettings(newSettings);
+        if (newSettings) {
+            updateSettings(newSettings);
 
-        localSettings = newSettings;
-      }
+            localSettings = newSettings;
+        }
 
-      return  localSettings;
+        return localSettings;
     }
 
 });
 
-function updateSettings(settings){
-      console.log(settings);
+function updateSettings(settings) {
+    console.log(settings);
 
-      if(player){
-          console.log('ya un  player');
-        if( localSettings.url !== settings.url){
-          player.loadVideoById(settings.url);
+    if (player) {
+        console.log('ya un  player');
+        if (localSettings.url !== settings.url) {
+            player.loadVideoById(settings.url);
         }
-        if( localSettings.videoTime !== settings.videoTime){
-          player.seekTo(settings.videoTime);
+        if (localSettings.videoTime !== settings.videoTime) {
+            player.seekTo(settings.videoTime);
         }
-        if( localSettings.state !== settings.state){
-          switch (settings.state) {
-            case 'play':
-              player.playVideo();
-              break;
-            case 'pause':
-              player.pauseVideo();
-              break;
-            case 'finish':
-                // console.log('status finsi');
-                // onYouTubeIframeAPIReady();
-                // Meteor.call('nextVideo');
-                break;
-            case 'wait':
+        if (localSettings.state !== settings.state) {
+            switch (settings.state) {
+                case 'play':
+                    player.playVideo();
+                    break;
+                case 'pause':
+                    player.pauseVideo();
+                    break;
+                case 'finish':
+                    // console.log('status finsi');
+                    // onYouTubeIframeAPIReady();
+                    // Meteor.call('nextVideo');
+                    break;
+                case 'wait':
 
-              break;
+                    break;
 
-          }
+            }
         }
 
 
-      }else{
+    } else {
         console.log('pas de player');
 
 
 
-          if(rdy){
-          console.log('sans onYouTubeIframeAPIReady');
-          player = new YT.Player('player', {
-                 videoId: settings.url,
-                 playerVars: {'controls': 0 },
-                 events: {
-                   'onStateChange': onStateChange,
-                   'onReady' : onPlayerReady,
-                 }
-               });
-              if(!time_update){
+        if (rdy) {
+            console.log('sans onYouTubeIframeAPIReady');
+            player = new YT.Player('player', {
+                videoId: settings.url,
+                playerVars: {
+                    'controls': 0
+                },
+                events: {
+                    'onStateChange': onStateChange,
+                    'onReady': onPlayerReady,
+                }
+            });
+            if (!time_update) {
                 time_update = Meteor.setInterval(updateBar, 1000);
-              }
-        }else {
-
-          onYouTubeIframeAPIReady = function(){
-            console.log('onYouTubeIframeAPIReady');
-            if(!player){
-              player = new YT.Player('player', {
-                     videoId: localSettings.url,
-                     playerVars: {'controls': 0 },
-                     events: {
-                       'onStateChange': onStateChange,
-                       'onReady' : onPlayerReady,
-                     }
-                   });
             }
+        } else {
 
-              rdy=true;
+            onYouTubeIframeAPIReady = function() {
+                console.log('onYouTubeIframeAPIReady');
+                if (!player) {
+                    player = new YT.Player('player', {
+                        videoId: localSettings.url,
+                        playerVars: {
+                            'controls': 0
+                        },
+                        events: {
+                            'onStateChange': onStateChange,
+                            'onReady': onPlayerReady,
+                        }
+                    });
+                }
+
+                rdy = true;
             };
 
 
             try {
-                  console.log('on essaye');
-                  if (YT && !player){
+                console.log('on essaye');
+                if (YT && !player) {
                     console.log('paseer aessaye');
                     console.log(settings);
                     player = new YT.Player('player', {
-                           videoId: settings.url,
-                           playerVars: {'controls': 0 },
-                           events: {
-                             'onStateChange': onStateChange,
-                             'onReady' : onPlayerReady,
-                           }
-                         });
-                         console.log(player);
-                    rdy=true;
-                  }
-              }
-              catch(err) {
-                  console.log(err);
-              }
-              time_update = Meteor.setInterval(updateBar, 1000);
+                        videoId: settings.url,
+                        playerVars: {
+                            'controls': 0
+                        },
+                        events: {
+                            'onStateChange': onStateChange,
+                            'onReady': onPlayerReady,
+                        }
+                    });
+                    console.log(player);
+                    rdy = true;
+                }
+            } catch (err) {
+                console.log(err);
+            }
+            time_update = Meteor.setInterval(updateBar, 1000);
 
         }
 
-  }
+    }
 }
 
 
 onPlayerReady = function(e) {
-  e.target.playVideo();
-  e.target.setVolume(50);
-  setUpSettings(e);
+    e.target.playVideo();
+    e.target.setVolume(50);
+    setUpSettings(e);
 
 
 };
 
-onStateChange = function (event) {
-  switch (event.data) {
-    case YT.PlayerState.ENDED:
-      console.log('ENDED');
-        Meteor.call('finishVideo');
+onStateChange = function(event) {
+    switch (event.data) {
+        case YT.PlayerState.ENDED:
+            console.log('ENDED');
+            Meteor.call('finishVideo');
 
-      break;
-    case YT.PlayerState.PAUSED:
-      console.log('PAUSED');
+            break;
+        case YT.PlayerState.PAUSED:
+            console.log('PAUSED');
 
-      if(localSettings.state === 'play'){
-        player.playVideo();
-      }
-      break;
-     case YT.PlayerState.PLAYING:
-      console.log('PLAYIN');
+            if (localSettings.state === 'play') {
+                player.playVideo();
+            }
+            break;
+        case YT.PlayerState.PLAYING:
+            console.log('PLAYIN');
 
-      if(localSettings.state === 'pause'){
-        player.pauseVideo();
-      }
-      break;
+            if (localSettings.state === 'pause') {
+                player.pauseVideo();
+            }
+            break;
 
-  }
+    }
 };
 
-setUpSettings = function(e){
-      if(localSettings){
-        if(localSettings.state == 'play'){
-          jumpToCurrentTime(e);
+setUpSettings = function(e) {
+    if (localSettings) {
+        if (localSettings.state == 'play') {
+            jumpToCurrentTime(e);
+        } else if (localSettings.state == 'pause') {
+            e.target.seekTo(localSettings.videoTime);
+            e.target.pauseVideo();
+        } else if (localSettings.state == 'stop') {
+
         }
-        else if(localSettings.state == 'pause'){
-          e.target.seekTo(localSettings.videoTime);
-          e.target.pauseVideo();
-        }else if(localSettings.state == 'stop'){
-
-        }
-      }
+    }
 
 
-  //     time_update = Meteor.setInterval(updateBar, 1000);
+    //     time_update = Meteor.setInterval(updateBar, 1000);
 
-        return true;
+    return true;
 };
 
-function updateBar(){
-  if (player) {
-    $('#progress-bar').val((player.getCurrentTime() / player.getDuration()) * 100);
-  }else{
-    console.log('no player');
-  }
+function updateBar() {
+    if (player) {
+        $('#progress-bar').val((player.getCurrentTime() / player.getDuration()) * 100);
+    } else {
+        console.log('no player');
+    }
 }
 
 
-function changeState(state){
-  switch(state) {
-      case 'play':
-          player.playVideo();
-          break;
-      case 'pause':
+function changeState(state) {
+    switch (state) {
+        case 'play':
+            player.playVideo();
+            break;
+        case 'pause':
 
-          player.pauseVideo();
-          break;
+            player.pauseVideo();
+            break;
 
-      case 'stop':
+        case 'stop':
 
-          player.stopVideo();
-          break;
-  }
-  return true;
+            player.stopVideo();
+            break;
+    }
+    return true;
 }
 
 
 
-function jumpToCurrentTime(e){
- return  Meteor.call('getCurrentTime',function(error,result){
-   if (error) {
-     return error;
-   }
-   else{
-     console.log(result);
-     return e.target.seekTo(result);
-   }
- });
+function jumpToCurrentTime(e) {
+    return Meteor.call('getCurrentTime', function(error, result) {
+        if (error) {
+            return error;
+        } else {
+            console.log(result);
+            return e.target.seekTo(result);
+        }
+    });
 }
